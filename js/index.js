38051503167a93b4b80782020298bcdb325403c7
Mau Acosta <chafito@live.com>
2021-06-23 14:58:37 -0500

    First commit of first version of portfolio

---
+++ js/index.js
@@ -0,0 +1,85 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
+
+scroll.init();
+
+barba.hooks.after(() => {
+  console.log("hola");
+  scroll.init();
+});
+
+barba.init({
+  sync: true,
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async;
+        pageTransition();
+        await delay(1000);
+        done();
+      },
+      async enter(data) {
+        animateContent();
+        secondAnimated();
+      },
+      async once(data) {
+        animateContent();
+        secondAnimated();
+      },
+    },
+  ],
+});
+
+function pageTransition() {
+  var tl = gsap.timeline();
+
+  tl.to(".loading-screen", {
+    duration: 1.2,
+    width: "100%",
+    left: "0%",
+    ease: "Expo.easeInOut",
+  });
+
+  tl.to(".loading-screen", {
+    duration: 1,
+    width: "100%",
+    left: "100%",
+    ease: "Expo.easeInOut",
+    delay: 0.3,
+  });
+
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+function animateContent() {
+  var tl = gsap.timeline();
+  tl.from(".animated", {
+    duration: 1,
+    y: 100,
+    opacity: 0,
+    stagger: 0.4,
+    delay: 0.5,
+  });
+}
+
+function secondAnimated() {
+  var tl = gsap.timeline();
+  tl.from(".second-animated", {
+    duration: 1,
+    y: 100,
+    opacity: 0,
+    stagger: 0.4,
+    delay: 0.75,
+  });
+}
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
